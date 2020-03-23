b1be9de07b981245125b0c4b85ce8b6d40f13a4b
Cody <cody.griffith94@gmail.com>
2020-03-23 04:04:28 +0000

    Reset to appdirectory

--- AppDirectory/AppDirectory/asgi.py
+++ AppDirectory/AppDirectory/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for FinanceCalculatorApp project.
+ASGI config for AppDirectory project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FinanceCalculatorApp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AppDirectory.settings')
 
 application = get_asgi_application()
--- AppDirectory/AppDirectory/wsgi.py
+++ AppDirectory/AppDirectory/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for FinanceCalculatorApp project.
+WSGI config for AppDirectory project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FinanceCalculatorApp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AppDirectory.settings')
 
 application = get_wsgi_application()
