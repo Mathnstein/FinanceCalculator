7b4466a9cb898e330c3e4c38f94a3b5105a9065b
Cody G <cody.griffith94@gmail.com>
2020-03-30 00:28:11 -0700

    Minor input style tweak

--- AppDirectory/AppDirectory/settings.py
+++ AppDirectory/AppDirectory/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#1*pw_q^tuax=a5p5-dw$=$7rr1u(2c_9(jiac61tfmp%3n49)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["mathnstein.pythonanywhere.com", "127.0.0.1"]
 
