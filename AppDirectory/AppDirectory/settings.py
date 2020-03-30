8ff55c0c8f892d8beb93ab678111f45eafd5ff64
Cody G <cody.griffith94@gmail.com>
2020-03-29 23:31:17 -0700

    Test2 Still trying to fix css issue

--- AppDirectory/AppDirectory/settings.py
+++ AppDirectory/AppDirectory/settings.py
@@ -119,7 +119,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "/FinanceMath/static"),
-]
-STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "FinanceMath","/static/")
+STATIC_URL = "/static/"
+
